0543ad5d1186cfa0171b8656f3b09653caa12f45
Walker Alfaro Trelles <walkeralfarotrelles@gmail.com>
2024-01-31 02:29:10 -0500

    add copytext and fix validation script

--- app.js
+++ app.js
@@ -2,71 +2,113 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // interface elements
   const textInput = document.querySelector('#text_input');
-  const formInput = document.querySelector('#form_input');
   const btnEncrypt = document.querySelector('#btn_encrypt');
   const btnDecrypt = document.querySelector('#btn_decrypt');
   const btnCopyText= document.querySelector('#btn_copy');
   const outputText = document.querySelector('#output_text');
-
+  
   // events
-  textInput.addEventListener('input', validate);
   btnEncrypt.addEventListener('click', encrypting);
   btnDecrypt.addEventListener('click', decrypting);
+  btnCopyText.addEventListener('click', copyText);
+  
 
-
-
-  function validate(e) {
-    const textToValidate = e.target.value;
+  function validate() {
+    const text = textInput.value;
     const pattern = /^[a-z\s]+$/;
-    const patternValidation = pattern.test(textToValidate);
-
-    if( (textToValidate.trim() == '') || !patternValidation ) {
-      // no habilitar botones de encriptación ni encriptar nada
-      console.log('no se puede encriptar');
+    const patternValidation = pattern.test(text);
+    
+    if( (text.trim() == '') || !patternValidation ) {
+      return false;
     } else {
-      // habilitar botones de encriptación y la opción de encriptar
-      console.log('si se puede encriptar');
+      return text;
+    }
+  }
+
+  function encrypting() {
+    const validationResult = validate();
+    if(validationResult) {
+      const textArray = [...validationResult];
+      textArray.forEach((letter, index, array) => {
+          switch (true) {
+  
+            case letter === 'a':
+              array[index] = 'ai'
+              break;
+            case letter === 'e':
+              array[index] = 'enter'
+              break;
+            case letter === 'i':
+              array[index] = 'imes'
+              break;
+            case letter === 'o':
+              array[index] = 'ober'
+              break;
+            case letter === 'u':
+              array[index] = 'ufat'
+              break;
+            default:
+              array[index] = letter;
+          }
+      });
+      const encryptedText = textArray.join('');
+  
+      // show encrypted text
+      showText(encryptedText);
+  
+      // clean input text of textarea
+      cleanInputText();
     }
 
+    return;
+
+
   }
 
-  function encrypting(text) {
-    const textArray = [...text];
-    textArray.forEach((letter, index, array) => {
-        switch (true) {
+  function decrypting() {
 
-          case letter === 'a':
-            array[index] = 'ai'
-            break;
-          case letter === 'e':
-            array[index] = 'enter'
-            break;
-          case letter === 'i':
-            array[index] = 'imes'
-            break;
-          case letter === 'o':
-            array[index] = 'ober'
-            break;
-          case letter === 'u':
-            array[index] = 'ufat'
-            break;
-          default:
-            array[index] = letter;
-        }
-    });
-    const encryptedText = textArray.join('')
-    return encryptedText;
+    const validationResult = validate();
+    if(validationResult) {
+      const decryptedText = validationResult.replace(/enter/g, 'e')
+                            .replace(/imes/g, 'i')
+                            .replace(/ai/g, 'a')
+                            .replace(/ober/g, 'o')
+                            .replace(/ufat/g, 'u');
+      // show decrypted text
+      showText(decryptedText);
+  
+      // clean input text of textarea
+      cleanInputText();
+    }
+
+    return;
   }
 
-  function decrypting(text) {
-    const decryptedText = text.replace(/enter/g, 'e')
-                          .replace(/imes/g, 'i')
-                          .replace(/ai/g, 'a')
-                          .replace(/ober/g, 'o')
-                          .replace(/ufat/g, 'u');
-    return decryptedText;
+  function showText(ciphertext) {
+    outputText.textContent = ciphertext;
   }
 
+  function cleanInputText() {
+    textInput.value = '';
+  }
+
+  async function copyText() {
+    try {
+      await navigator.clipboard.writeText(outputText.textContent);
+    } catch (error) {
+      prompt('hubo un error al copiar al portapapeles');
+    }
+  }
+
+  function showElement() {
+    //TODO: mostrar elementos HTML
+  }
+
+  function hideElement() {
+    //TODO: ocultar elementos HTML
+  }
+
+  
 
 
 
